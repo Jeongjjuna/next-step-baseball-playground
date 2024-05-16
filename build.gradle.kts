5a6afdbbbe2e6ec199d4ead76f7919725e2e13e9
jeongjihun <wlgns2234@naver.com>
2024-05-16 16:32:04 +0900

    chore: 의존성 라이브러리 이름 수정
    - 잘못된 이름의 라이브러리로 인해 테스트 빌드가 실패함

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-    testImplementation("io.kotest:kotest-assertions-score:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
     runtimeOnly("io.kotest:kotest-framework-datatest-jvm:5.6.2")
 }
 
