6aa9c0b14cdea00b6cc05084eb5b46cfde790491
jeongjihun <wlgns2234@naver.com>
2024-05-17 00:10:23 +0900

    refactor: String을 상수로 분리

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,13 +1,19 @@
 package baseball.view
 
 class InputView {
+
+    companion object {
+        private const val START_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        private const val INPUT_MESSAGE = "숫자를 입력해 주세요 : "
+    }
+
     fun inputRestart(): String {
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        println(START_MESSAGE)
         return readln()
     }
 
     fun inputNumber(): String {
-        print("숫자를 입력해 주세요 : ")
+        print(INPUT_MESSAGE)
         return readln()
     }
 }
--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -3,24 +3,34 @@ package baseball.view
 import baseball.domian.GameStatus
 
 class OutputView {
+
+    companion object {
+        private const val GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        private const val BALL_MESSAGE = "%d볼 "
+        private const val STRIKE_MESSAGE = "%d스트라이크"
+        private const val NOTHING_MESSAGE = "낫싱"
+    }
+
     fun printGameOver() {
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println(GAME_OVER_MESSAGE)
     }
 
     fun printResult(gameStatus: GameStatus) {
         if (gameStatus.isNothing()) {
-            println("낫싱")
+            println(NOTHING_MESSAGE)
             return
         }
 
         val ballMessage = when (gameStatus.ball) {
             0 -> ""
-            else -> "${gameStatus.ball}볼 "
+            1, 2, 3 -> BALL_MESSAGE.format(gameStatus.ball)
+            else -> throw IllegalArgumentException("[Error] 볼 개수는 0, 1, 2, 3개 중에 하나여야 합니다.")
         }
 
         val strikeMessage = when (gameStatus.strike) {
             0 -> ""
-            else -> "${gameStatus.strike}" + "스트라이크"
+            1, 2, 3 -> STRIKE_MESSAGE.format(gameStatus.strike)
+            else -> throw IllegalArgumentException("[Error] 스트라이크 개수는 0, 1, 2, 3개 중에 하나여야 합니다.")
         }
 
         println(ballMessage + strikeMessage)
