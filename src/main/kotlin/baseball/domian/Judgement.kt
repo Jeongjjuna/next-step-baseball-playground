2619a4f944dc5005e188ce485281c2b0bb9b8e8a
jeongjihun <wlgns2234@naver.com>
2024-05-20 13:50:37 +0900

    refactor: when을 사용하도록 변경

--- src/main/kotlin/baseball/domian/Judgement.kt
+++ src/main/kotlin/baseball/domian/Judgement.kt
@@ -2,16 +2,9 @@ package baseball.domian
 
 class Judgement {
 
-    fun judge(balls: Balls, targetBall: Ball, targetPosition: Int): ResultType {
-
-        if (balls.isStrike(targetBall, targetPosition)) {
-            return ResultType.STRIKE
-        }
-
-        if (balls.isBall(targetBall, targetPosition)) {
-            return ResultType.BALL
-        }
-
-        return ResultType.NOTHING
+    fun judge(balls: Balls, targetBall: Ball, targetPosition: Int) = when {
+        balls.isStrike(targetBall, targetPosition) -> ResultType.STRIKE
+        balls.isBall(targetBall, targetPosition) -> ResultType.BALL
+        else -> ResultType.NOTHING
     }
 }
